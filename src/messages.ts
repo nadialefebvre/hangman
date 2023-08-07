200c929db8aff17e83f85752987ddfe02925a1a2
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-08-07 12:01:59 +0200

    set up messages

--- src/messages.ts
+++ src/messages.ts
@@ -1,10 +1,43 @@
 import { defineMessages } from "react-intl"
 
-export const scope = "app.start"
+const globalScope = "app.global"
+const startScope = "app.start"
+const playScope = "app.play"
+const winScope = "app.win"
+const loseScope = "app.lose"
 
 export default defineMessages({
+  restartButton: {
+    id: `${globalScope}.restartButton`,
+    defaultMessage: "Restart",
+  },
+  wordMessage: {
+    id: `${globalScope}.wordMessage`,
+    defaultMessage: "The word was ",
+  },
+
   instructions: {
-    id: `${scope}.instructions`,
-    defaultMessage: "This is the default message",
+    id: `${startScope}.instructions`,
+    defaultMessage: "Choose a category or get a random word",
+  },
+  // word types??
+
+  counterPlural: {
+    id: `${playScope}.counterPlural`,
+    defaultMessage: " attempts left",
+  },
+  counterSingular: {
+    id: `${playScope}.counterSingular`,
+    defaultMessage: " attempt left",
+  },
+
+  winMessage: {
+    id: `${winScope}.winMessage`,
+    defaultMessage: "You WON!",
+  },
+
+  loseMessage: {
+    id: `${loseScope}.loseMessage`,
+    defaultMessage: "You LOST!",
   },
 })
