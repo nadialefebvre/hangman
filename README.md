da95c05388e9a10b4731a4d1555acd91bd4080cb
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-09-18 11:34:42 +0200

    start transition into fullstack, with server and client

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# hangman
+# Hangman
 
 (work in progress)
---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import express from "express"
+
+const app = express()
+
+const port = process.env.PORT || 8080
+
+app.get("/", (req, res) => {
+  res.send("This is my server!")
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
