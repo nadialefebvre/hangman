1fc7a303b8f769f4d9ecf8bbc9dbf986755eb52f
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-10-23 13:36:18 +0200

    move globalstyle to its own file

---
+++ client/src/App/GlobalStyle.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components/macro"
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: "Press Start 2P", cursive;
+    /* font-family: "VT323", monospace;
+    font-family: "DotGothic16", sans-serif;
+    letter-spacing: 8px; */
+  }
+
+  body {
+    margin: 0;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
