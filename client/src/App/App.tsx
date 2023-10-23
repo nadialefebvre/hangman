92141ce1b5d4a3e9439fc8e7eac5bd61232e3067
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-10-23 13:36:40 +0200

    move App into its folder

---
+++ client/src/App/App.tsx
@@ -0,0 +1,47 @@
+import React, { useContext, useEffect } from "react"
+import { IntlProvider } from "react-intl"
+
+import Footer from "../components/Footer"
+import GameGrid from "../components/GameGrid"
+import Header from "../components/Header"
+import Play from "../components/Play"
+import Result from "../components/Result"
+import Start from "../components/Start"
+import { GameContext } from "../game/context"
+import { GuessStatus } from "../game/types"
+import { updateFavicon } from "../utils/favicon"
+import { getLocaleData } from "../utils/translation"
+import { GlobalStyle } from "./GlobalStyle"
+
+const App: React.FC = () => {
+  const { state } = useContext(GameContext)
+  const { language, alphabet, result, phase, randomWord } = state
+
+  console.log(randomWord)
+
+  const localeData = getLocaleData(language)
+
+  const wrongCount: number = alphabet.filter(
+    (item) => item.guessStatus === GuessStatus.Wrong
+  ).length
+
+  const stepToUse = phase === "RESULT" ? 8 : wrongCount
+  useEffect(() => {
+    updateFavicon(stepToUse, phase, result)
+  }, [stepToUse, phase, result])
+
+  return (
+    <IntlProvider locale={language} messages={localeData}>
+      <GlobalStyle />
+      <GameGrid result={result} phase={phase} wrongCount={wrongCount}>
+        <Header wrongCount={wrongCount} />
+        {phase === "START" && <Start />}
+        {phase === "PLAY" && <Play />}
+        {phase === "RESULT" && <Result />}
+        <Footer />
+      </GameGrid>
+    </IntlProvider>
+  )
+}
+
+export default App
